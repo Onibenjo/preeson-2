27a1d7977bd4a79a66bb196d967cf8bad1c23bd7
Oni Benjamin <onibenjo@gmail.com>
2020-09-24 22:26:11 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -3,33 +3,36 @@ import '../styles/css/plugins.css'
 import '../styles/css/bootstrap.min.css'
 import '../styles/style.css'
 import cartReducer from '../components/reducers/cartReducer'
-import App, {Container} from 'next/app'
+// import App, {Container} from 'next/app'
 
-import withRedux from "next-redux-wrapper";
-import React from 'react-dom'
+import { createWrapper }from 'next-redux-wrapper'
+import React from 'react'
 import { Provider } from 'react-redux'
 import { createStore } from 'redux'
-class MyApp extends App {
-  
-  static async getInitialProps({Component, ctx}) {
-    const pageProps = Component.getInitialProps ? await Component.getInitialProps(ctx) : {};
-     return {pageProps: pageProps};
-    }
-    
-    render(){
-      const {Component, pageProps, store} = this.props;
-  return (
-       
-      <Provider store={store}>
-        <Component {...pageProps} />
-      </Provider>
-    
-  
-    )
-    }
-}
+
+// classbased
+// class MyApp extends App {
+//   static async getInitialProps(ctx) {
+//     const appProps = await App.getInitialProps(ctx);
+//     return { ...appProps };
+//   }
+//   render(){
+//    const {Component, pageProps, store} = this.props;
+//     return (
+//         <Provider store={store}>
+//           <Component {...pageProps} />
+//         </Provider>
+//       )
+//     }
+// }
+
+//function based
+const MyApp = ({ Component, pageProps, store }) =>  <Provider store={store}><Component {...pageProps} /> </Provider>
+
 const store = createStore(cartReducer);
 const makeStore = () => store;
-export default MyApp
+// export default MyApp
 // wrapper.withRedux(makeStore, {debug:true})(MyApp)
 //you can remove the comment the from the above and use it with the export
+const wrapper = createWrapper(makestore, {debug: true})
+export default wrapper.withRedux(MyApp)
